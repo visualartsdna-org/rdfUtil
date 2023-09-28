4ff19b1a93ecd0c246810b7ada00c9e4b5fb653c
rspates <rspates@users.noreply.github.com>
2023-09-28 10:16:47 -0400

    Update mvnJarRepo.bat

--- mvnJarRepo.bat
+++ mvnJarRepo.bat
@@ -1,3 +1,5 @@
 
 rem https://stackoverflow.com/questions/4955635/how-to-add-local-jar-files-to-a-maven-project
 mvn install:install-file -Dfile=target\rdfUtil-1.0.0.jar -DgroupId=org.visualartsdna -DartifactId=rdfUtil -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true
+
+pause
