815ff77446f5864af9eac77de331ac86ca4a508a
rspates <rspates@users.noreply.github.com>
2024-01-02 14:42:27 -0500

    Update JsonRdfUtilTest.groovy
    	// loads latest keep takeout
    	// filters on labels
    	// captures takeout concepts
    	// creates ttl

--- src/main/java/rdf/util/JsonRdfUtilTest.groovy
+++ src/main/java/rdf/util/JsonRdfUtilTest.groovy
@@ -5,22 +5,82 @@ import static org.junit.jupiter.api.Assertions.*
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 import org.junit.jupiter.api.Test
+import groovy.io.FileType
 
 class JsonRdfUtilTest {
-	
+
+	def prefixMap = [
+		definition:"skos",
+		scopeNote:"skos",
+		historyNote:"skos",
+	]
+
+	//		add to prefix map
+	//		pick most recent zip from folder
+
+	// https://stackoverflow.com/questions/3662144/recursive-listing-of-all-files-matching-a-certain-filetype-in-groovy
+	@Test
+	void testRecentZip() {
+		def path = "C:/temp/Takeout/rspates.art"
+		def dir = new File(path)
+		long dt=0
+		File f
+		
+		
+		new File(path).traverse(type: groovy.io.FileType.FILES, nameFilter: ~/.*\.zip$/) { it ->
+			if (it.lastModified() > dt) {
+				dt = it.lastModified()
+				f = it
+			}
+		}
+			println "$f, ${new Date(f.lastModified()).format('EEE MMM dd hh:mm:ss a yyyy')}"
+	}
+	@Test
+	void test() {
+		def path = "C:/temp/Takeout/rspates.art"
+
+		long dt=0
+		File f
+		
+		
+		new File(path).traverse(type: groovy.io.FileType.FILES, nameFilter: ~/.*\.zip$/) { it ->
+			if (it.lastModified() > dt) {
+				dt = it.lastModified()
+				f = it
+			}
+		}
+			println "$f, ${new Date(f.lastModified()).format('EEE MMM dd hh:mm:ss a yyyy')}"
+		def zipFile = new java.util.zip.ZipFile(f)
+
+		zipFile.entries().findAll { !it.directory }
+		.findAll {
+			(""+it).endsWith(".json")
+		}
+		.each {
+			def m = new JsonSlurper().parseText( zipFile.getInputStream(it).text )
+			m.findAll{
+				filter (it, "publish") && !filter (it, "tsh")
+			}.each{
+				//println m.textContent
+				def m2= writeTko2Map(m.title, m.textContent)
+				def s = makeTtl(m2)
+				println "$s\n\n"
+			}
+		}
+	}
+
 
 	@Test
-	void test0() {
-		def fn = ""
-		def m = new JsonSlurper().parse(new File(fn))
-		def sb = new StringBuilder()
-		def prefix = "tko:"
-		new JsonRdfUtil().jsonToTtl(m, sb, prefix)
-		println ""+sb
+	void testRecentZip0() {
+		def path = "C:/temp/Takeout/rspates.art"
+		def dir = new File(path)
+		new File(path).traverse(type: groovy.io.FileType.FILES, nameFilter: ~/.*\.zip$/) { it ->
+			println "$it, ${new Date(it.lastModified()).format('EEE MMM dd hh:mm:ss a yyyy')}"
+		}
 	}
 
 	@Test
-	void test() {
+	void test0() {
 		def fn = "C:/temp/Takeout/rspates.art/takeout-20230928T154928Z-001.zip"
 
 		def zipFile = new java.util.zip.ZipFile(new File(fn))
@@ -28,47 +88,30 @@ class JsonRdfUtilTest {
 		zipFile.entries().findAll { !it.directory }
 		.findAll {
 			(""+it).endsWith(".json")
-			//.findAll { (""+it).contains("Extinction") && (""+it).endsWith(".json")
 		}
 		.each {
-			//println it
 			def m = new JsonSlurper().parseText( zipFile.getInputStream(it).text )
-			//println JsonOutput.prettyPrint(zipFile.getInputStream(it).text)
 			m.findAll{
 				filter (it, "publish") && !filter (it, "tsh")
 			}.each{
 				//println m.textContent
-				def m2= writeTko2Ttl(m.title, m.textContent)
-//				m2.each{k,v->
-//					println "$k\n\t$v"
-//				}
+				def m2= writeTko2Map(m.title, m.textContent)
 				def s = makeTtl(m2)
 				println "$s\n\n"
 			}
 		}
 	}
-	
+
 	@Test
 	void testJson() {
 		def fn = "C:/temp/Takeout/rspates.art/Extinction Statement.json"
 
-			def m = new JsonSlurper().parseText( new File(fn).text )
-			//println JsonOutput.prettyPrint(zipFile.getInputStream(it).text)
-				//println m.textContent
-				def m2= writeTko2Ttl(m.title, m.textContent)
-//				m2.each{k,v->
-//					println "$k\n\t$v"
-//				}
-				def s = makeTtl(m2)
-				println "$s\n\n"
+		def m = new JsonSlurper().parseText( new File(fn).text )
+		def m2= writeTko2Map(m.title, m.textContent)
+		def s = makeTtl(m2)
+		println "$s\n\n"
 	}
-	
-	def nsMap = [
-		definition:"skos",
-		scopeNote:"skos",
-		historyNote:"skos",
-		]
-	
+
 	def makeTtl(m) {
 		def s = """
 @prefix tko: <http://visualartsdna.org/takeout#> .
@@ -85,15 +128,15 @@ class JsonRdfUtilTest {
 				k1 != "definition"
 			}.each{k1,v1->
 				s += """
-			${nsMap[k1]}:$k1 "${v1.trim()}" ;
+			${prefixMap[k1]}:$k1 "${v1.trim()}" ;
 				"""
 			}
-			s +="."
+			s +="\t\t."
 		}
 		s
 	}
 
-	def writeTko2Ttl(title,text) {
+	def writeTko2Map(title,text) {
 		def topConcept = false
 		def newConcept = false
 		def fndText = false
@@ -103,7 +146,6 @@ class JsonRdfUtilTest {
 		def m=[:]
 		m[title]=[:]
 		text.eachLine{
-			//println "===$it"
 			if (it.trim() == "") {
 				blankcnt++
 				if (blankcnt>=2) {
@@ -114,7 +156,6 @@ class JsonRdfUtilTest {
 					m[title]["definition"]=txt
 					fndText = false
 					txt = ""
-					//blankcnt = 0
 				}
 				else if (fndText && newConcept) {
 					newConcept = false
@@ -144,7 +185,7 @@ class JsonRdfUtilTest {
 		}
 		m
 	}
-	
+
 
 	static def camelCase(s) {
 		s.replaceAll( /( )([A-Za-z0-9])/, {
@@ -160,5 +201,15 @@ class JsonRdfUtilTest {
 				m2.name==criteria
 			}
 		false
+	}
+
+	@Test
+	void test00() {
+		def fn = ""
+		def m = new JsonSlurper().parse(new File(fn))
+		def sb = new StringBuilder()
+		def prefix = "tko:"
+		new JsonRdfUtil().jsonToTtl(m, sb, prefix)
+		println ""+sb
 	}
 }
